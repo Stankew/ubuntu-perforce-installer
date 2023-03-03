8db4b78057d3c90f0c4aaceb505ee7b99dc05691
Stanok1 <konats96@gmail.com>
2023-03-04 00:14:17 +0100

    Update install-perforce.sh

--- install-perforce.sh
+++ install-perforce.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 distro=$(cat /etc/os-release | grep VERSION_CODENAME | awk -F= '{print $2}')
-wget -qO - https://package.perforce.com/perforce.pubkey | sudo apt-key add -
+wget -qO - https://package.perforce.com/perforce.pubkey | gpg --dearmor | sudo tee /usr/share/keyrings/perforce.gpg
 echo "deb [signed-by=/usr/share/keyrings/perforce.gpg] https://package.perforce.com/apt/ubuntu  $distro  release" > /etc/apt/sources.list.d/perforce.list
 apt-get update
 apt install helix-p4d -y
